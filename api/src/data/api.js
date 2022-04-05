edcd65755fa1ed06c8a0c5c2a030374d239a8254
joaquin-garcia-plante <joaquingarciaplante@gmail.com>
2022-04-05 13:50:42 -0300

    update

--- api/src/data/api.js
+++ api/src/data/api.js
@@ -1,20 +1,16 @@
 require("dotenv").config();
 const axios = require("axios");
 const { detailRouteApi, mainRouteApi } = require("../providers/api");
-const { API_KEY } = process.env;
+// const { API_KEY } = process.env;
 
 const getApiRecipes = async () => {
-  const apiInfo = await axios.get(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
-  );
+  const apiInfo = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=1db02e94b5b54f3e8974e5c8e0e972f5&addRecipeInformation=true&number=100`);
   let result = apiInfo.data.results;
   result = mainRouteApi(result);
   return result;
 };
 const getApiRecipeDetail = async (id) => {
-  const apiInfo = await axios.get(
-    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`
-  );
+  const apiInfo = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=1db02e94b5b54f3e8974e5c8e0e972f5`);
   let result = detailRouteApi(apiInfo.data);
   return result;
 };
